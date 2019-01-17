98c1d206a6ead43f74e56eb3bb18aec47b3ca648
Brian Lee <ibltsandwich@gmail.com>
2019-01-17 15:21:27 -0500

    Finish adding register for users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
 
 router.get('/test', (req, res) => res.json({ msg: "this is the users route" }));
+
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        // Throw an error if user already exists
+        return res.status(400).json({ email: "A user has already registered with this address"})
+      } else {
+        // Otherwise create a new user
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
+        })
+        // Salt and hash password
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          })
+        })
+      }
+    })
+});
 
 module.exports = router;
 
