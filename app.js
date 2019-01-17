c8b26c674b3978aa690b56c289e3f02cf5a0a9f3
Brian Lee <ibltsandwich@gmail.com>
2019-01-17 17:29:06 -0500

    Finish Passport middleware setup

--- app.js
+++ app.js
@@ -6,19 +6,20 @@ const app = express();
 const db = require('./config/keys').mongoURI;
 const users = require('./routes/api/users');
 const events = require('./routes/api/events');
-
+const passport = require('passport');
+require('./config/passport')(passport);
 
 mongoose
   .connect(db)
   .then(() => console.log('connected to mongodb successfully'))
   .catch(err => console.log(err));
 
-app.use("/api/users", users);
-app.use("/api/events", events);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use("/api/users", users);
+app.use("/api/events", events);
 
-app.get('/', (req, res) => res.send("Helsdfd!"));
+app.use(passport.initialize());
 
 const port = process.env.PORT || 5000;
 
