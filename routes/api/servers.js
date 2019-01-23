bb4cbb7bba5cc8833b320d7d7f286c2aaf63aa39
Brian Lee <ibltsandwich@gmail.com>
2019-01-23 18:58:13 -0500

    Add create and fetch servers functionality

--- routes/api/servers.js
+++ routes/api/servers.js
@@ -3,21 +3,22 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 const Server = require('../../models/Server');
-const validateServerCreation = require("../../validation/server");
+// const validateServerCreation = require("../../validation/server");
 
 router.get('/servers', (req, res) => res.json({ msg: "this is the servers route" }));
 
 router.post('/', passport.authenticate('jwt', { session: false }),
   (req, res) => {
-    const { errors, isValid } = validateServerCreation(req.body);
+    // const { errors, isValid } = validateServerCreation(req.body);
 
-    if	(!isValid) {
-      return res.status(400).json(errors);
-    }
+    // if	(!isValid) {
+    //   return res.status(400).json(errors);
+    // }
     
     const newServer = new Server({
       name: req.body.name,
-      owner: req.user.id
+      owner: req.user.id,
+      users: [req.user.id]
     })
     
     newServer.save().then(server => res.json(server));
@@ -25,11 +26,11 @@ router.post('/', passport.authenticate('jwt', { session: false }),
 
 router.get('/', passport.authenticate('jwt', { session: false }),
   (req, res) => {
-    const { errors, isValid } = validateServerCreation(req.body);
+    // const { errors, isValid } = validateServerCreation(req.body);
 
-    if (!isValid) {
-      return res.status(400).json(errors);
-    }
+    // if (!isValid) {
+    //   return res.status(400).json(errors);
+    // }
 
     Server.find({ users: req.user.id })
       .then(servers => res.json(servers))
--- validation/server.js
+++ validation/server.js
@@ -1,22 +1,22 @@
-const Validator = require("validator");
-const isEmpty = require("./is-empty");
+// const Validator = require("validator");
+// const isEmpty = require("./is-empty");
 
-module.exports = function validateServerCreation(data) {
-  let errors = {};
+// module.exports = function validateServerCreation(data) {
+//   let errors = {};
 
-  data.name = !isEmpty(data.name) ? data.name : "";
-  data.owner = !isEmpty(data.owner) ? data.owner : "";
+//   data.name = !isEmpty(data.name) ? data.name : "";
+//   data.owner = !isEmpty(data.owner) ? data.owner : "";
 
-  if (!Validator.isLength(data.name, { min: 1, max: 20 })) {
-    errors.name = "Server must be between 1 and 20 characters";
-  }
+//   if (!Validator.isLength(data.name, { min: 1, max: 20 })) {
+//     errors.name = "Server must be between 1 and 20 characters";
+//   }
 
-  if (Validator.isEmpty(data.name)) {
-    errors.name = "Server needs a name";
-  }
+//   if (Validator.isEmpty(data.name)) {
+//     errors.name = "Server needs a name";
+//   }
 
-  return {
-    errors,
-    isValid: isEmpty(errors)
-  };
-};
+//   return {
+//     errors,
+//     isValid: isEmpty(errors)
+//   };
+// };
